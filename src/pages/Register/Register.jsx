97e68c5bc85c2e10160c0413acb140d2361835e6
Sanam911 <sanamashna11@gmail.com>
2024-04-28 00:39:46 +0600

    eye icon added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,8 @@
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 
 
@@ -8,6 +10,7 @@ const Register = () => {
 
     const [registerError, setRegisterError] = useState('');
     const [registerSuccess, setRegisterSuccess] = useState('');
+    const [showPass, setShowPass] = useState(false);
 
     const { createUser } = useContext(AuthContext);
 
@@ -22,11 +25,11 @@ const Register = () => {
         const password = form.get('password');
         console.log(name, photo, email, password);
 
-        if(password.length < 6){
+        if (password.length < 6) {
             setRegisterError('Password should be atleast 6 characters or longer');
             return;
         }
-        else if (!/[a-z]+.*[A-Z]|[A-Z]+.*[a-z]/.test(password)){
+        else if (!/[a-z]+.*[A-Z]|[A-Z]+.*[a-z]/.test(password)) {
             setRegisterError('Your password should contain at least 1 uppercase & 1 lowercase character in it ');
             return;
         }
@@ -54,12 +57,11 @@ const Register = () => {
 
     return (
 
-
         <div className="hero min-h-screen bg-base-200 ">
-            
 
-            <div className="card w-full max-w-sm shadow-2xl bg-base-100">
-            {
+
+            <div className="w-3/2 lg:w-1/2  shadow-2xl bg-base-100 p-4 rounded-lg">
+                {
                     registerSuccess &&
                     <div role="alert" className="alert alert-success text-white">
                         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
@@ -67,7 +69,7 @@ const Register = () => {
                     </div>
                 }
                 <h2 className="text-3xl font-bold text-violet-500 text-center mt-4">Register Here!</h2>
-                <form onSubmit={handleRegister} className="card-body">
+                <form onSubmit={handleRegister} >
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
@@ -92,17 +94,27 @@ const Register = () => {
                             name="photo"
                             className="input input-bordered" required />
                     </div>
-                    <div className="form-control">
+                    <div className="relative">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="password"
+                        <input type={showPass ? "text" : "password"} placeholder="password"
                             name="password"
-                            className="input input-bordered" required />
+                            className="input input-bordered w-full" required />
+                        <span className="absolute top-12  right-4 text-violet-500 text-2xl" onClick={() => setShowPass(!showPass)}>
+                            {
+                                showPass ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                            }
+                        </span>
+                    </div>
+
+                    <div>
                         <label className="label">
                             <a href="#" className="label-text-alt link link-hover">Already registered? <Link className="text-violet-500" to='/login'>Login</Link></a>
                         </label>
                     </div>
+
+
                     <div className="form-control mt-6">
                         <button className="btn bg-violet-300 text-violet-500">Register</button>
 
@@ -112,7 +124,7 @@ const Register = () => {
                     registerError &&
                     <p className="text-xl text-red-600 font-bold text-center mb-4">{registerError}</p>
                 }
-                
+
             </div>
 
         </div>
